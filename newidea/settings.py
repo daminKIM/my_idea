797efed8054cf43724221aed3afdd37f90aa966d
daminKIM <ekals4407@gmail.com>
2019-07-27 18:11:00 +0900

    db

--- newidea/settings.py
+++ newidea/settings.py
@@ -170,4 +170,4 @@ LOGIN_REDIRECT_URL = '/'
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env
+DATABASES['default'].update(db_from_env)
